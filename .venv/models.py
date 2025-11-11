8926a4735eacf86cbb15de24d1d27b624b3ba44a
Cloudy32 <r.bondar29@gmail.com>
2025-11-12 00:03:47 +0300

    End models Start dev views

--- .venv/models.py
+++ .venv/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+date = datetime.now()
+
 class Expense():
 
     def __init__(self, amount, discription, date):
@@ -11,7 +13,7 @@ class Expense():
     def __str__(self):
         return f"Дата: {self.date.strftime('%Y-%m-%d')}, Описание: {self.discription}, Сумма: {self.amount}"
 
-    def get_info(self):
+    def _get_info(self):
         return f'{self.amount} потрачено на {self.discription} за {self.date}'
 
 
@@ -22,17 +24,37 @@ class ExpenseTracker():
         self.expenses = []
 
     def add_expense(self, amount, discription, date):
+
         ex = Expense(amount, discription, date)
         self.expenses.append(ex)
+        return 'расход был добавлен'
+
+    def _remove_expense(self, index):
+
+        if 0 <= index < len(self.expenses):
+            self.expenses.pop(index)
+        else:
+            return 'Ошибка: индекс вне деапозона!'
+        return 'Расход был удален!'
+
+    def _total_expenses(self):
+
+        total = sum(expense.amount for expense in self.expenses)
+        return f'Общая сумма расходов: {total}'
+
+    def _get_expenses(self):
+
         return [str(expense) for expense in self.expenses]
 
-    def remove_expense(self, index):
-        self.expenses.pop(index)
-        return f'Расход был удалне'
 
-e = ExpenseTracker()
-print(e.add_expense(1000, 'Покушать', datetime.now()))
-print(e.expenses)
-print(e.remove_expense(0))
-print(e.expenses)
+
+# e = ExpenseTracker()
+# print(e.add_expense(1000, 'Покушать', datetime.now()))
+# print(e.get_expenses())
+# print(e.add_expense(5000, 'Путешествия', date))
+# print(e.add_expense(5034, 'Дом', date))
+# print(e.remove_expense(0))
+# print(e.get_expenses())
+# print(e.total_expenses())
+
 
--- .venv/views.py
+++ .venv/views.py
@@ -1,1 +1,34 @@
-from prettytable import PrettyTable
+#from prettytable import PrettyTable
+from models import ExpenseTracker
+
+expense = ExpenseTracker
+
+#функционал для вывода информации
+
+def display_expenses():
+    pass
+
+def show_total():
+    pass
+
+#Обработка ввода от пользователя
+
+def new_expense():
+    pass
+
+#Функционал для работы приложения
+
+def show_list_expenses():
+    pass
+
+def show_all_expense(expense):
+    result = expense._total_expenses()
+    return result
+
+def get_user_expense():
+    pass
+
+def chose_func():
+    pass
+
+show_all_expense()
